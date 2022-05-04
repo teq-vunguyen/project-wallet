878d7c15a7752d55c87bb7ac23b9680f146dc4e1
Vu Nguyen <vunguyen@teqnological.asia>
2022-05-04 11:12:27 +0700

    remove code

--- pages/demo.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import DemoReduxToolkit from "@src/components/DemoReduxToolkit";
-
-const DemoPage: React.FC = () => {
-  return <DemoReduxToolkit />;
-};
-export default DemoPage;
